1034a678faeff067b4b71024e4ef466a0b835206
andresabadia <andres.abadia@id-ex.de>
2019-03-18 16:35:47 +0100

    andres part

--- app/src/main/java/com/example/android/testradar/GettingStarted.java
+++ app/src/main/java/com/example/android/testradar/GettingStarted.java
@@ -122,7 +122,7 @@ public class GettingStarted extends Activity implements LocationListener {
         mapView.map().layers().add(locationLayer);
 
     }
-//test  fhfg
+//test  fhfg gn
     @Override
     protected void onResume() {
         super.onResume();
